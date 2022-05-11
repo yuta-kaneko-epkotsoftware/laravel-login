8452cb7c1ef40b776c20f4244da186611a081867
Kaneko Yuta <yuta.kaneko@epkotsoftware.co.jp>
2022-05-11 16:57:25 +0900

    ログインフォーム作成、バリデーション設定

---
+++ resources/views/login/login_form.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログインフォーム</title>
+    <!-- Scripts -->
+    <script src="{{asset('js/app.js')}}" defer></script>
+    <!-- Styles -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
+    <link href="{{asset('css/signin.css')}}" rel="stylesheet">
+</head>
+<body>
+ <form class="form-signin" method="POST" action="{{route('login')}}">
+  @csrf
+  <h1 class="h3 mb-3 font-weight-normal">ログインフォーム</h1>
+  @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+  @endif
+  
+  <label for="inputEmail" class="sr-only">Email address</label>
+  <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Email address" required autofocus>
+  <label for="inputPassword" class="sr-only">Password</label>
+  <input type="password" id="inputPassword" name="password" class="form-control" placeholder="Password" required>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
+ </form>
+
+</body>
+</html>
